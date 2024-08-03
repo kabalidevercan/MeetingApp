f1b3b25ffdcf1d7ed9da0189b56d639f87cf4a77
kabalidevercan <kabalidevercan@gmail.com>
2024-08-03 17:13:41 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+
+
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+
+            // ViewBag.Selamlama = saat > 12 ? "Iyi gunler" : "Gunaydin";
+            // ViewBag.Isim = "Ercan Bey";
+
+            ViewData["Selamlama"] = saat > 12 ? "Iyi gunler" : "Gunaydin";
+            ViewData["Username"] = "Ercan Bey";
+
+            var meetingInfo = new MeetingInfo()
+            {
+                Id = 1,
+                Location = "Istanbul, Abc Kongre Merkezi",
+                Date = new DateTime(2024, 01, 20, 20, 0, 0),
+                NumberOfPeople = 100
+
+            };
+
+
+            return View(meetingInfo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//{controller=Home}/{action=Index}/id?
+//app.MapDefaultControllerRoute();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
